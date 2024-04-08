8eed019bcd98efaab6fd8456c7e9af52faeab74e
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 20:59:31 +1000

    Update 1_Manage_Menu.py

--- pages/1_Manage_Menu.py
+++ pages/1_Manage_Menu.py
@@ -5,7 +5,7 @@ def display_current_menu(df):
   st.subheader('Current Menu')
   for category, group_df in df.groupby('Category'):
     with st.expander(category):
-      st.dataframe(group_df[['Item','Price (AUD)','Level	Description']])
+      st.dataframe(group_df[['Item','Price (AUD)','Description']])
       
 if __name__ == "__main__":
   df = initiate()
