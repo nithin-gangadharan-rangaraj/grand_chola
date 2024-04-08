0df5aa3029accc30c5b7fe385831c1912c51b925
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 21:17:30 +1000

    Update 1_Manage_Menu.py

--- pages/1_Manage_Menu.py
+++ pages/1_Manage_Menu.py
@@ -6,10 +6,9 @@ def display_current_menu(df):
   groups_df = dict()
   for category, group_df in df.groupby('Category'):
     with st.expander(category):
-      groups_df[category] = st.dataeditor(group_df)
+      groups_df[category] = st.data_editor(group_df)
   return groups_df
       
 if __name__ == "__main__":
   df = initiate()
-  st.dataframe(df)
   groups_df = display_current_menu(df)
