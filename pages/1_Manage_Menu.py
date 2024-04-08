925e1112cccf7c64538ee2e8edfc87f247bceae8
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 21:26:19 +1000

    Update 1_Manage_Menu.py

--- pages/1_Manage_Menu.py
+++ pages/1_Manage_Menu.py
@@ -1,14 +1,20 @@
 import streamlit as st
 from auxillaries import *
 
-def display_current_menu(df):
-  st.subheader('Grand Chola - Menu')
+def update_menu(groups_df, update_container):
+  update_container.warning("Looks like you've updated your menu. Finalise by clicking the button!")
+  update_container.button('Click here to update')
+
+def display_current_menu(df, update_container):
   groups_df = dict()
   for category, group_df in df.groupby('Category'):
     with st.expander(category):
-      groups_df[category] = st.data_editor(group_df)
+      groups_df[category] = st.data_editor(group_df, on_change = update_menu, args = (groups_df, update_container))
   return groups_df
       
 if __name__ == "__main__":
   df = initiate()
-  groups_df = display_current_menu(df)
+  st.subheader('Grand Chola - Menu')
+  update_container = st.container()
+  groups_df = display_current_menu(df, update_container)
+  
