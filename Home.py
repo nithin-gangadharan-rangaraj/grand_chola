57af898aff11d0c4fcac898e83dc1fcd4f528aee
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 13:40:07 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,15 @@
 import streamlit as st
 from auxillaries import *
 
+def display_menu(df):
+  for category, group_df in df.groupby('Category'):
+    st.subheader(f"{category.upper()}:", divider = 'orange')
+    for idx, row in group_df.iterrows():
+      st.write(f'**{row['Item'].capitalize()}**')
+      st.write(f'Price: {row['Price (AUD)']}')
+      st.write(f'{row['Description']}')
+    st.divider()
+
 if __name__ == "__main__":
   df = initiate()
-  st.write(df)
-  st.write("Menu")
+  display_menu(df)
