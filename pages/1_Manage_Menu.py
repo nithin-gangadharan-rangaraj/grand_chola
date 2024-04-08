4abf9ef203ac9bfd783d892fe6938aad7a82f379
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 21:48:04 +1000

    Update 1_Manage_Menu.py

--- pages/1_Manage_Menu.py
+++ pages/1_Manage_Menu.py
@@ -2,7 +2,7 @@ import streamlit as st
 from auxillaries import *
 import pandas as pd
 
-def update_menu(groups_df, update_container):
+def update_menu(groups_df, update_container, conn):
   with update_container:
     st.warning("Looks like you've made some changes. Finalise by clicking the button! Leave it be otherwise.")
     if st.button('Click here to update'):
@@ -15,17 +15,22 @@ def update_menu(groups_df, update_container):
       st.experimental_rerun()
     st.divider()
 
-def display_current_menu(df, update_container):
+def display_current_menu(df, update_container, conn):
   groups_df = dict()
   for category, group_df in df.groupby('Category'):
     with st.expander(category):
-      groups_df[category] = st.data_editor(group_df, on_change = update_menu, args = (groups_df, update_container))
+      groups_df[category] = st.data_editor(group_df, on_change = update_menu, args = (groups_df, update_container, conn))
   return groups_df
+
+def get_conn_df(conn):
+  df = conn.read(ttl = 0)
+  return df
       
 if __name__ == "__main__":
   conn = initiate()
+  df = get_conn_df()
   st.subheader('Grand Chola - Menu')
   update_container = st.container()
-  groups_df = display_current_menu(df, update_container)
+  groups_df = display_current_menu(df, update_container, conn)
   combined_df = pd.concat(groups_df.values())
   st.dataframe(combined_df)
