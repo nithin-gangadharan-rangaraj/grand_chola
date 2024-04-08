a712a0dcdc9ab07ee4b184f850acb6e02f7da373
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 12:57:14 +1000

    Update auxillaries.py

--- auxillaries.py
+++ auxillaries.py
@@ -1,28 +1,41 @@
-from google.oauth2 import service_account
+
 import streamlit as st
-import gspread
-import pandas as pd
+from streamlit_gsheets import GSheetsConnection
+
+def initiate()
+  # Create a connection object.
+  conn = st.connection("gsheets", type=GSheetsConnection)
+  
+  df = conn.read(worksheet="Sheet1",
+                 ttl="0")
+  return df
+
+# from google.oauth2 import service_account
+# import streamlit as st
+# import gspread
+# import pandas as pd
 
   
-def initiate():
-  if 'gsheet' in st.session_state:
-      gsheet = st.session_state['gsheet']
-  else:
-      # Load service account credential
-      service_acc = st.secrets["gcp_service_account"]
+# def initiate():
+#   if 'gsheet' in st.session_state:
+#       gsheet = st.session_state['gsheet']
+#   else:
+#       # Load service account credential
+#       service_acc = st.secrets["gcp_service_account"]
       
           
-      credentials = service_account.Credentials.from_service_account_info(
-      service_acc,
-      scopes=[
-          "https://www.googleapis.com/auth/spreadsheets",
-          "https://www.googleapis.com/auth/drive"
-      ],
-      )
+#       credentials = service_account.Credentials.from_service_account_info(
+#       service_acc,
+#       scopes=[
+#           "https://www.googleapis.com/auth/spreadsheets",
+#           "https://www.googleapis.com/auth/drive"
+#       ],
+#       )
   
-      # Authorize gspread client
-      gs = gspread.authorize(credentials)
-      st.write(st.secrets['sheet_name'])
-      gsheet = gs.open(st.secrets['sheet_name'])
-      st.session_state['gsheet'] = gsheet
-  return gsheet
+#       # Authorize gspread client
+#       gs = gspread.authorize(credentials)
+#       st.write(st.secrets['sheet_name'])
+#       gsheet = gs.open(st.secrets['sheet_name'])
+#       st.session_state['gsheet'] = gsheet
+#   return gsheet
+
