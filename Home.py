81530190dda4e5f55de172f958f0e8ab84acc276
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 14:33:44 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ st.markdown(style, unsafe_allow_html=True)
 
 def display_menu(df):
   for category, group_df in df.groupby('Category'):
-    st.subheader(f"{category.upper()}:", divider = 'orange')
+    st.header(f"{category.upper()}:", divider = 'orange')
     for idx, row in group_df.iterrows():
       st.write(f"**{row['Item'].capitalize()} - {row['Price (AUD)']}**")
       st.write(f"{row['Description']}")
@@ -26,7 +26,7 @@ def display_menu(df):
 
 def display_logo():
   st.title('GRAND CHOLA INDIAN RESTAURRANT')
-  st.write('**Discover :orange[authentic Indian cuisine], reimagined.**')
+  st.subheader('**Discover :orange[authentic Indian cuisine], reimagined.**')
   # st.image('chola.png', width = 200)
 
 if __name__ == "__main__":
