6db2a22ec7e0727c20d10c6ef98cbc4beae5a95b
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 21:17:05 +1000

    Update 1_Manage_Menu.py

--- pages/1_Manage_Menu.py
+++ pages/1_Manage_Menu.py
@@ -2,12 +2,14 @@ import streamlit as st
 from auxillaries import *
 
 def display_current_menu(df):
-  st.subheader('Current Menu')
+  st.subheader('Grand Chola - Menu')
+  groups_df = dict()
   for category, group_df in df.groupby('Category'):
     with st.expander(category):
-      st.dataframe(group_df[['Item','Price (AUD)','Description']])
+      groups_df[category] = st.dataeditor(group_df)
+  return groups_df
       
 if __name__ == "__main__":
   df = initiate()
   st.dataframe(df)
-  display_current_menu(df)
+  groups_df = display_current_menu(df)
