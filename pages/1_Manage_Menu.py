862b38df7d411316b63df5c3fdda0e7c56e1f3f1
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 20:59:10 +1000

    Update 1_Manage_Menu.py

--- pages/1_Manage_Menu.py
+++ pages/1_Manage_Menu.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from auxillaries import *
 
+def display_current_menu(df):
+  st.subheader('Current Menu')
+  for category, group_df in df.groupby('Category'):
+    with st.expander(category):
+      st.dataframe(group_df[['Item','Price (AUD)','Level	Description']])
+      
 if __name__ == "__main__":
-  st.write('Menu Management')
   df = initiate()
+  display_current_menu(df)
