030731779d579461d1195f7f83fd4deccfe53f5f
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 23:28:00 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,6 @@ from auxillaries import *
 st.set_page_config(page_title="Grand Chola")
 
 if __name__ == "__main__":
-  st.image('chola.png', width = 300)
+  st.image('chola.png', width = 500)
   st.header('Welcome', divider = 'orange')
   st.write('Click the side bar on the top left corner to start.')
