9a237f3c4a3def9b87ceeac8cbf60fe4657f3f05
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 13:40:37 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,9 +5,9 @@ def display_menu(df):
   for category, group_df in df.groupby('Category'):
     st.subheader(f"{category.upper()}:", divider = 'orange')
     for idx, row in group_df.iterrows():
-      st.write(f'**{row['Item'].capitalize()}**')
-      st.write(f'Price: {row['Price (AUD)']}')
-      st.write(f'{row['Description']}')
+      st.write(f"**{row['Item'].capitalize()}**")
+      st.write(f"Price: {row['Price (AUD)']}")
+      st.write(f"{row['Description']}")
     st.divider()
 
 if __name__ == "__main__":
