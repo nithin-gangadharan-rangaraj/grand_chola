9d57c3b5716709d3b952d3cebbd4149caaa73b2a
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-08 13:58:14 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from auxillaries import *
 
+st.set_page_config(page_title="Grand Chola - Menu",layout='wide')
+
+
 style = '''
     <style>
         header {visibility: hidden;}
