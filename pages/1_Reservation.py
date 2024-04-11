83866ad6287281869ad70bfc0c19f692670e5881
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-11 18:36:11 +1000

    Update 1_Reservation.py

--- pages/1_Reservation.py
+++ pages/1_Reservation.py
@@ -3,26 +3,30 @@ from auxillaries import *
 from datetime import datetime, timedelta
 import pytz
 
+def reshape_df(df):
+  # Reshape the dataframe
+  melted_df = pd.melt(df, id_vars=['Name', 'Group size', 'Number'], var_name='Time', value_name='Availability')
+  
+  # Convert 'Time' column to datetime format
+  melted_df['Time'] = pd.to_datetime(melted_df['Time'].str.replace(' hrs', ''), format='%H:%M')
+  
+  # Filter out rows where availability is not null
+  result_df = melted_df.dropna(subset=['Availability'])
+  
+  # Group by Name, Group size, and Number, and select the row with the minimum availability time
+  result_df = result_df.groupby(['Name', 'Group size', 'Number']).apply(lambda x: x.loc[x['Time'].idxmin()]).reset_index(drop=True)
+  result_df['Time'] = pd.to_datetime(result_df['Time']).dt.strftime('%H:%M')
+  result_df = result_df[['Name', 'Group size', 'Number', 'Time']]
+  return result_df
+
 def show_reservations(conn):
   date = st.date_input("📅 Reservation Date", value="default_value_today" , format="DD/MM/YYYY").strftime('%d/%m/%Y')
   if date in worksheet_names(conn):
     df = read_worksheet(conn, date).dropna(how = "all")
     if len(df) > 0:
-      # Reshape the dataframe
-      melted_df = pd.melt(df, id_vars=['Name', 'Group size', 'Number'], var_name='Time', value_name='Availability')
-      
-      # Convert 'Time' column to datetime format
-      melted_df['Time'] = pd.to_datetime(melted_df['Time'].str.replace(' hrs', ''), format='%H:%M')
-      
-      # Filter out rows where availability is not null
-      result_df = melted_df.dropna(subset=['Availability'])
-      
-      # Group by Name, Group size, and Number, and select the row with the minimum availability time
-      result_df = result_df.groupby(['Name', 'Group size', 'Number']).apply(lambda x: x.loc[x['Time'].idxmin()]).reset_index(drop=True)
-      result_df['Time'] = pd.to_datetime(result_df['Time']).dt.strftime('%H:%M')
-      result_df = result_df[['Name', 'Group size', 'Number', 'Time']]
-      
-      st.dataframe(result_df,
+      st.subheader("Here are your reservations:")
+      df = reshape_df(df)    
+      st.dataframe(df,
                   column_config={
                                   "Number": st.column_config.NumberColumn(
                                       format="%d",
