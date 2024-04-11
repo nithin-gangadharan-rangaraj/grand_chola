33bd8adb536ea64c7b3b0d8b65ebead457212390
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-11 19:26:12 +1000

    Update 1_Reservation.py

--- pages/1_Reservation.py
+++ pages/1_Reservation.py
@@ -14,7 +14,7 @@ def reshape_df(df):
   result_df = melted_df.dropna(subset=['Availability'])
   
   # Group by Name, Group size, and Number, and select the row with the minimum availability time
-  result_df = result_df.groupby(['Name', 'Group size', 'Number']) #.apply(lambda x: x.loc[x['Time'].idxmin()]).reset_index(drop=True)
+  result_df = result_df.groupby(['Name', 'Group size', 'Number']).apply(lambda x: x.loc[x['Time'].idxmin()]).reset_index(drop=True)
 
   # result_df['Time'] = pd.to_datetime(result_df['Time']).dt.strftime('%H:%M')
 
