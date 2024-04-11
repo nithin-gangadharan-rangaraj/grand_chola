0db5527bf97f6ffd451bc0b6f49b6f70d7d8b2ea
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-04-11 18:14:50 +1000

    Update 1_Reservation.py

--- pages/1_Reservation.py
+++ pages/1_Reservation.py
@@ -5,8 +5,10 @@ import pytz
 
 def show_reservations(conn):
   date = st.date_input("📅 Reservation Date", value="default_value_today" , format="DD/MM/YYYY").strftime('%H:%M')
+  st.write(worksheet_names(conn))
   if date in worksheet_names(conn):
     df = read_worksheet(conn, date)
+    st.write(df)
     if len(df) > 0:
       st.datafame(df)   
     else:
